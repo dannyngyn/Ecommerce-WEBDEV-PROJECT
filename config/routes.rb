0b4cc7b948178a0760f5aa16ced2c0cee6f2e617
Danny Nguyen <danny.nguyen772@gmail.com>
2024-04-16 20:59:40 -0500

    Added a user controller with login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'user/login'
   get 'checkout/show'
   get 'fish/index'
   get 'cart/shop'
+  get 'user/login'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
